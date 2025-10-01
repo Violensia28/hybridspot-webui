4ae6fd0c13d75741028ed60a1248c9bcaafa78e8
Violensia28 <107469569+Violensia28@users.noreply.github.com>
2025-10-01 20:57:07 +0700

    Add files via upload

--- include/ui_assets.h
+++ include/ui_assets.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 extern const char INDEX_HTML[] PROGMEM;
 extern const char CSS_MAIN[] PROGMEM;
 extern const char CSS_THEME[] PROGMEM;
@@ -16,4 +15,3 @@ extern const char JS_SETTINGS[] PROGMEM;
 extern const char JS_OTA[] PROGMEM;
 extern const char SVG_LOGO[] PROGMEM;
 extern const char OTA_HTML[] PROGMEM;
-
